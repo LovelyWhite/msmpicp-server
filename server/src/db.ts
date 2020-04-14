2ceb32d78aa8017e3f374e76e920a0fc713121bf
LovelyWhite <icezxn@qq.com>
2020-04-14 18:40:32 +0800

    update

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 export async function openMongoose() {
   try {
-    //   "mongodb://39.105.171.169:27017/msmpicp"
-    await mongoose.connect("mongodb://39.105.171.169:27017/msmpicp", {
+    //   "mongodb://admin:Mishiweilai123@39.105.171.169:27017"
+    await mongoose.connect("mongodb://admin:Mishiweilai123@39.105.171.169:27017/msmpicp?authSource=admin&readPreference=primary&gssapiServiceName=mongodb&appname=MongoDB%20Compass&ssl=false", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,72 @@
-import React from "react";
-export default () => {
+import React from 'react';
+import { Form, Input, Button, Checkbox, Row, Col } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import "antd/dist/antd.css";
+const NormalLoginForm = () => {
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
-    <div style={{}}>
-      <div style={{ width: 30 }}>demo</div>
-    </div>
+    <Row justify="center">
+      <Col span={12}>
+        <img src={process.env.PUBLIC_URL + "/intro.png"} alt="" />
+      </Col>
+      <Col span={12}>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="">
+              Forgot password
+        </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+        </Button>
+        Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
   );
 };
+
+export default NormalLoginForm;
